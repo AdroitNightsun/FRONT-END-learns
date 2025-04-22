b375be092c086ff6f392574685e4d6990ecb354d
Nightsun <kumarkaran65176@gmail.com>
2025-04-22 20:11:24 +0530

    started new proj

---
+++ Exercises and qs/ext11.js
@@ -0,0 +1,36 @@
+/*
+
+bussiness anme generator
+
+adjectives:
+crazy
+amazing
+fire
+
+shop name:
+engine
+foods
+garments
+
+another word:
+bros
+limited
+hub
+*/
+
+let adjectives ={
+    1 : "crazy",
+    2 : "amazing",
+    3 : "fire"
+}
+
+let shop_name = {
+    1 : "engine",
+    2 : "foods",
+    3 : "garments"
+}
+let another_word = {
+    1 : "bros",
+    2 : "limited",
+    3 : "hub"
+}    
---
+++ JS string/strings.js
@@ -0,0 +1,13 @@
+let naei = "broke";
+let friend = "beach";
+
+console.log("his name is "+ naei +" and his friend's name is "+ friend);
+console.log(`his name is ${naei} and his friend's name is ${friend}`); // template literal
+let a = "shivam";
+
+console.log(a.length); // length of string
+console.log(a.toUpperCase()); // convert to upper case
+console.log(a.toLowerCase()); // convert to lower case
+console.log(a.slice(1, 4)); // slice the string from index 1 to 4
+console.log(a.replace("s", "5")); // replace s with 5
+console.log(a.concat("? A good boy indeed !!!", " he is a player"));
